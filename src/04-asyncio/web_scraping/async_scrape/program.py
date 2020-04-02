3cdf4bba04138e3f64c9f9bec52f42c4b6001c2a
Ross Roberts <ross123roberts@gmail.com>
2020-04-02 18:59:38 -0400

    second async implementation

--- src/04-asyncio/web_scraping/async_scrape/program.py
+++ src/04-asyncio/web_scraping/async_scrape/program.py
@@ -1,18 +1,14 @@
-import asyncio
-
-import aiohttp
 import bs4
 from colorama import Fore
-
-# Older versions of python require calling loop.create_task() rather than on asyncio.
-# Make this available more easily.
-global loop
+import aiohttp
+import asyncio
 
 
 async def get_html(episode_number: int) -> str:
     print(Fore.YELLOW + f"Getting HTML for episode {episode_number}", flush=True)
 
     url = f'https://talkpython.fm/{episode_number}'
+    # resp = requests.get(url)
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
@@ -32,26 +28,18 @@ def get_title(html: str, episode_number: int) -> str:
 
 
 def main():
-    global loop
     loop = asyncio.get_event_loop()
     loop.run_until_complete(get_title_range())
     print("Done.")
-
-
-async def get_title_range_old_version():
-    # Please keep this range pretty small to not DDoS my site. ;)
-    for n in range(150, 160):
-        html = await get_html(n)
-        title = get_title(html, n)
-        print(Fore.WHITE + f"Title found: {title}", flush=True)
 
 
 async def get_title_range():
     # Please keep this range pretty small to not DDoS my site. ;)
 
     tasks = []
+
     for n in range(150, 160):
-        tasks.append((n, loop.create_task(get_html(n))))
+        tasks.append( (n,asyncio.create_task(get_html(n))))
 
     for n, t in tasks:
         html = await t
